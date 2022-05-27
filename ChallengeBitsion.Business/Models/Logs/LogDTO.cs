5c8fa7e9fa279d3859acdd889dc38bad4b7b01c2
César <cesar_delse@hotmail.com>
2022-05-26 22:06:37 -0300

    Add log files

---
+++ ChallengeBitsion.Business/Models/Logs/LogDTO.cs
@@ -0,0 +1,13 @@
+﻿using ChallengeBitsion.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeBitsion.Business.Models.Logs
+{
+    public class LogDTO : EntityBase
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ ChallengeBitsion.Business/Models/Logs/LogEditDTO.cs
@@ -0,0 +1,14 @@
+﻿using ChallengeBitsion.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeBitsion.Business.Models.Logs
+{
+    public class LogEditDTO : EntityBase
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
